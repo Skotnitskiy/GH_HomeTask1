f90bf8c7c6a371aa9b3f7d04d983af25b1a94eb6
Skotnytskij <one2011@yandex.ru>
2013-10-09 23:00:00 +0300

    firstproj

--- First/src/com/gh/first/MainActivity.java
+++ First/src/com/gh/first/MainActivity.java
@@ -1,23 +1,44 @@
 package com.gh.first;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		android.app.FragmentManager  fm = getFragmentManager();
+		android.app.Fragment startFragment = new android.app.Fragment();
+		fm.beginTransaction().add(R.id.fragment_placeholder, startFragment);
+		fm.beginTransaction().commit();
+		
 
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	public void onSelectFragment(View view) {
+		
+		Fragment newFragment = new Fragment();
+		
+		if (view == findViewById(R.id.btnStartFrag)) {
+			newFragment = new StartFragment();
+		} else if (view == findViewById(R.id.btnFrag1)) {
+			newFragment = new Fragment1();
+		} else if (view == findViewById(R.id.btnFrag2)) {
+			newFragment = new Fragment2();
+		} else {
+			newFragment = new StartFragment();
+		}
+		
+		FragmentTransaction tr = 
+		getFragmentManager().beginTransaction().replace(R.id.fragment_placeholder, newFragment);
+		tr.addToBackStack(null);
+		tr.commit();
+
+	}
 }
