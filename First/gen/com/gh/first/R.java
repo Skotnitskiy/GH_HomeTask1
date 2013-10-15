c03d2a8a078be9f3cac3b966f2a6d503b5b7d693
Serh <one2011@yandex.ru>
2013-10-15 21:12:17 +0300

    smartgit

--- First/gen/com/gh/first/R.java
+++ First/gen/com/gh/first/R.java
@@ -24,18 +24,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f070000;
-        public static final int btnFrag1=0x7f070002;
-        public static final int btnFrag2=0x7f070003;
-        public static final int btnStartFrag=0x7f070001;
-        public static final int fragment_placeholder=0x7f070004;
-        public static final int textView1=0x7f070005;
+        public static final int LinearLayout1=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int btnFrag1=0x7f080002;
+        public static final int btnFrag2=0x7f080003;
+        public static final int btnStartFrag=0x7f080001;
+        public static final int fragment_placeholder=0x7f080004;
+        public static final int textView1=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int fragment_1=0x7f030001;
         public static final int fragment_2=0x7f030002;
         public static final int start_fragment=0x7f030003;
+    }
+    public static final class menu {
+        public static final int main=0x7f070000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
--- First/src/com/gh/first/MainActivity.java
+++ First/src/com/gh/first/MainActivity.java
@@ -3,7 +3,6 @@ package com.gh.first;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 
@@ -13,15 +12,9 @@ public class MainActivity extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		FragmentManager fm = getSupportFragmentManager();
-		Fragment startFragment = new Fragment();
-		fm.beginTransaction().add(R.id.LinearLayout1, startFragment);
-		fm.beginTransaction().commit();
-
 	}
 
 	public void onSelectFragment(View view) {
-
 		Fragment newFragment = new Fragment();
 		if (view == findViewById(R.id.btnStartFrag)) {
 			newFragment = new StartFragment();
@@ -29,13 +22,10 @@ public class MainActivity extends FragmentActivity {
 			newFragment = new Fragment1();
 		} else if (view == findViewById(R.id.btnFrag2)) {
 			newFragment = new Fragment2();
-		} else {
-			newFragment = new StartFragment();
 		}
 
 		FragmentTransaction tr = getSupportFragmentManager().beginTransaction()
 				.replace(R.id.fragment_placeholder, newFragment);
-		tr.addToBackStack(null);
 		tr.commit();
 
 	}
